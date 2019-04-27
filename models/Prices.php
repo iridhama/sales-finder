cec645217c398dfe929cef0a25746e110f7cb7d2
ajay_pathak <ajay_pathak@modexbyte.com>
2019-04-27 11:10:57 +0530

    base models extended

---
+++ models/Prices.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\models;
+
+use app\models\base\Prices as BasePrices;
+
+class Prices extends BasePrices {
+
+}
---
+++ models/Stores.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\models;
+
+use app\models\base\Stores as BaseStores;
+
+class Stores extends BaseStores {
+
+}
